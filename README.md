c70b481ae630934030a3341c0e9f39ff724f4898
Moe1313 <anotherbidder13@yahoo.com>
2025-01-23 18:57:38 -0800

    Done with app.py file.Ready to commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Sprint4ProjectCars
+
+PROJECT TITLE:
+Sprint 4 - WEB Application to display and analyze Automobile Data from the vehicles_us.csv file
+
+OBJECTIVES:
+We will look at 1 csv file(vehicles_us.csv), and represent the data in this file as a dataframe called df_cars. Then we will attempt to clean and analyze the dataframe and make conclusions and create a web application using the data after it has been cleaned. The Web Application will contain an interactive dataframe that can be filtered by the end user. In addition, the web application will also have a Histogram chart and Scatter Plot chart that can be customized by the end user in real time.
+
+DESCRIPTION OF DATASETS:
+FILE 1: vehicles_us.csv: each row corresponds to a car that is for sale
+DATAFRAME: df_Cars
+'price': Price of car
+'model_year': Year car was made
+'model': Model of car,(Needs to be broken out to make and model)
+'condition': condition of car('good', 'like new', 'fair', 'excellent', 'salvage', 'new')
+'cylinders': number of cylinders each car has(6., 4., 8., nan, 5., 10., 3., 12.) Needs to be changed to type int
+'fuel': type of fuel the car uses('gas', 'diesel', 'other', 'hybrid', 'electric')
+'odometer': provides the mileage on the car
+'transmission': type of transmission(automatic vs manual or other)
+'type': what type of car('SUV', 'pickup', 'sedan', 'truck', 'coupe', 'van', 'convertible','hatchback', 'wagon', 'mini-van', 'other', 'offroad', 'bus')
+'paint_color': color of exterior of car
+'is_4wd': Specifies whether or not the car is the car 4 wheel drive(Needs to be of type string for "Yes" or "No" or "Unknown")
+'date_posted': Date the car was listed for sale on the site
+'days_listed': Number of days the car has been listed for sale
+
+PLAN ON HOW TO ACHIEVE OBJECTIVE GOALS:
+STEP 1: Import the file into python and analyze the data by assigning the contents to a dataframe. Then use dataframe methods such as describe(), info() or shape() to help get a big picture understanding of the data
+STEP 2: Clean the data within the dataframe using various methods and functions within pandas and python to Identify, remove or replace null values, make sure data types in columns are correct, remove duplicate rows, etc. One the data is cleaned, Explore the relationship between sedan, suv and coupe in relation to price and draw some basic conclusions. We will draw up a box plot and histogram to examine the relationship between these 3 car types and their respective price.
+STEP 3: Use the rest of this notebook to run test code to help create the app.py interactive web application. We will be using this juypter notebook in conjunction with visual studio code to develope the web app called app.py. Within this app, we will use streamlit package in conjunction with the github and render websites to help us port our app.py application to the web. Our app.py web application will contain the following functionality:
+The first part of the web application will have an interactive dataframe where end users can filter the dataframe in real time by make, car type, price and mileage. The second portion of this web app will contain an interactive histogram and scatter-plot chart. The histogram will have the ability to be customized and filtered by make, type, fuel and paint color in real time by the end user. The scatter-plot chart will have the ability to be customized and filtered by age, odometer, cylinders, model_year, and days listed in real time by the end user.
+PLEASE NOTE: This step will contain some scrap code that may or may not be used by the app.py application. The code in Step 3 is merely to be used as a test bed for app.py.
+
+Here are the files involved for this project:
+app.py -main code
+config.toml -website port configurations for render site to use
+requirements.txt - helps set the environment for app.py(package versions etc...)
+vehicles_us.csv - main datafile for that app.py web application
+
---
+++ app.py
@@ -0,0 +1,152 @@
+﻿import pandas as pd
+import numpy as np
+import streamlit as st 
+import plotly.express as px 
+
+df_orig = pd.read_csv(r"C:\PythonCode\Sprint4FinalProject\Sprint4ProjectCars\vehicles_us.csv")
+#Making copy of original dataframe and will use this dataframe copy for the analysis going forward
+df_Cars = df_orig.copy()
+#We will now clean up dataframe
+#Make sure datatypes are correct or make sense for each column and if they don't, fix it
+#lets change the year data type from float to int as there are no decimal values in a year 
+df_Cars["model_year"] = pd.to_numeric(df_Cars["model_year"], errors='coerce').astype('Int64')
+#lets change the cylinder data type from float to int as there are no decimal in the number of cylinders a car has
+df_Cars["cylinders"] = pd.to_numeric(df_Cars["cylinders"], errors='coerce').astype('Int64')
+#lets change the odometer data type from float to int as well
+df_Cars["odometer"] = pd.to_numeric(df_Cars["odometer"], errors='coerce').astype('Int64')
+#lets change the date_posted type from string to date/time  
+df_Cars['date_posted']=pd.to_datetime(df_Cars['date_posted'], format='%Y-%m-%d', errors='coerce')
+#let's convert the price column to type float as prices tend to have decimals
+df_Cars["price"]=df_Cars.price.astype(float)
+#Let's convert the is 4 wheel drive column to a yes or no, as it makes more sense compared to the current values of 1.0 and NaN.
+df_Cars["is_4wd"] = df_Cars["is_4wd"].apply(lambda x: "Yes" if x == 1.0 else "No")
+
+#Cleanup null values and Create New Columns, or break apart existing columns into separate columns if neccessary to enhance the dataset
+# we will replace all null values for the paint_color column to unknown
+df_Cars["paint_color"]=df_Cars["paint_color"].fillna("unknown")
+#the "make" column has both the make and model combined.  We need to split the values out into separate columns, make and model.
+#first lets replace all null values for the model column with "unknown unknown" as the first unknown is the make and the 2nd unknown is the model
+df_Cars["model"]=df_Cars["model"].fillna("unknown unknown")
+#next we split the model value and create another column called "Make"
+df_Cars[["make","model"]]=df_Cars["model"].str.split(" ", n=1, expand=True)
+#Now lets create a new column called age.  we will take the max of the date posted
+#and extract the year to get the most recent year based on this dataset, and subtract
+#the model_year column from it to get the relative age of the car based on this dataset
+df_Cars["age"] = df_Cars["date_posted"].max().year - df_Cars["model_year"]
+
+# As we can see, except for the model_year, cylinders, and odometer columns the data looks clean without null values.  
+# It might be prudent to keep the null values for the 3 columns mentioned above because we may want to see how no values in the model year,
+# number of cylinders, and odometer affect whether or not a car gets sold or how long it sits based on the days_listed column.  
+#lets change the make and model values to proper case just to make the data values look nicer
+df_Cars["model"]=df_Cars["model"].str.title()
+df_Cars["make"]=df_Cars["make"].str.title()
+#Let's reorder the columns so that make and model are next to each other and are also before the model_year cdataframe_columns
+# Get the current column order
+columns = list(df_Cars.columns)
+# Define the desired column order
+new_order = ['make', 'model'] + [col for col in columns if col not in ['make', 'model']]
+# Reorder the DataFrame columns
+df_Cars = df_Cars[new_order]
+
+
+#STEP 1, create lists that will feed our dropdownlist values for end users to select from in our Web Application
+
+Make_List = df_Cars["make"].unique()
+Make_List.sort()
+#want to add an "All" value as a choice incase end users want to select all. I will do this for some lists where applicable going forward.
+#Make_List.insert(0,"All") 
+Make_List=np.insert(Make_List, 0, "All")
+
+
+CarType_List = df_Cars["type"].unique()
+#We are allowing for an option for the end user to select all car types
+CarType_List=np.insert(CarType_List, 0, "All")
+# so now we have the following lists to use for dropdown selections for end users to pick from.  We will use the following lists for our program
+# for dropdown list selection Make_List, CarType_List for the first portion of our webapp
+
+
+# First portion of the Design of our Web App Begins now:
+# first we will encapsulate this portion of the code in a try except to capture any errors that may arise from the first portion of our code.
+try:
+    st.header("""Market of used Cars""")
+    st.write("""##### Filter the data below by make, type, price and mileage.""")
+    Maker_Selection=st.selectbox("Select an Automaker:",Make_List)
+    Type_Selection=st.selectbox("Select an Car type:",CarType_List)
+    # Since we added an "All" in the dropdown list, we have to right an if statement for all senarios where end user selects "All" from
+    # either Make selection, car type selection or both
+    if (Maker_Selection == "All") & (Type_Selection == "All") :
+        df_filtered = df_Cars
+    elif (Maker_Selection == "All") & (Type_Selection != "All"):
+        df_filtered = df_Cars[(df_Cars.type==Type_Selection)]
+    elif (Maker_Selection != "All") & (Type_Selection != "All"):
+        df_filtered = df_Cars[(df_Cars.make==Maker_Selection)&(df_Cars.type==Type_Selection)]
+    elif (Maker_Selection != "All") & (Type_Selection == "All"):
+        df_filtered = df_Cars[(df_Cars.make==Maker_Selection)]
+    #Let's get the min, max and avg price for the current selection
+
+    MinPrice = int(df_filtered["price"].min())
+    MaxPrice = int(df_filtered["price"].max())
+    AvgPrice = int(df_filtered["price"].mean())
+
+    # Format the values with dollar signs and commas
+    formatted_max_price = f"\\${MaxPrice:,.2f}"  # Dollar sign does not need escaping in Streamlit
+    formatted_min_price = f"\\${MinPrice:,.2f}"
+    formatted_avg_price = f"\\${AvgPrice:,.2f}"
+
+    # Display the formatted message in the Streamlit app
+    st.write(f"For the current selections, the min price is: {formatted_min_price}, the max price is: {formatted_max_price}, and the avg price is: {formatted_avg_price}")
+
+    # now let's set the min and max values for a price slider for end users to further narrow down their car search based on price
+    PriceRange = st.slider("Based on your selections above, you can filter down further based on price if you like. Please select your price range", value=(MinPrice,MaxPrice),min_value=MinPrice,max_value=MaxPrice)
+    
+    df_filteredPrice=df_filtered[(df_filtered["price"]>=PriceRange[0]) & (df_filtered["price"]<=PriceRange[1])]
+    
+    # now let's set the min and max values for a odometer slider for end users to further narrow down their car search based on mileage
+    #let's get the max and min values for the odometer from the df_filteredPrice dataframe and apply these values to set our mileage slider
+    MinM = int(df_filteredPrice["odometer"].min())
+    MaxM = int(df_filteredPrice["odometer"].max())
+    AvgM = int(df_filteredPrice["odometer"].mean())
+    MileageRange = st.slider("Based on your price selection above, you can filter further based on mileage if you like. Please select your mileage range", value=(MinM,MaxM),min_value=MinM,max_value=MaxM)
+   
+   # we create a new dataframe below called df_filteredMiles to reflect the records filted based on odometer mileage selected by the end user
+    df_filteredMiles=df_filteredPrice[(df_filteredPrice["odometer"]>=MileageRange[0]) & (df_filteredPrice["odometer"]<=MileageRange[1])]
+    df_filteredMiles
+    
+    
+    
+except Exception as e:
+    st.error(f"Sorry, no cars matched your selection choices. Please try a different selection of the automaker or car type.")
+
+
+
+
+#2nd Portion of our code starts here. We will be plotting a histogram and scatter plot chart:
+st.header("""Viewing Car Data by Histogram and Scatter plot Charts""")
+st.write("""##### Choose below the car characteristic and how it affects price for the Histogram Chart""")
+#HISTOGRAM CHART: We will plot a historgram chart to see how the following factors affect a car's price: fuel, type, paint_color, make
+Hist_DropdownChoice = ["make","type", "fuel","paint_color"]
+choice = st.selectbox("Select a car characteristic to compare against price:",Hist_DropdownChoice)
+Fig_Hist = px.histogram(df_Cars, x="price", color = choice, width=2000, height=500)
+Fig_Hist.update_layout(title="<b> {} vs Price</b>".format(choice))
+# we will limit the x axis range to 100,000 just to make the graph look bigger and better proportioned
+Fig_Hist.update_xaxes(range=[0, 100000])
+st.plotly_chart(Fig_Hist)
+
+#SCATTER PLOT CHART:
+# we will plot a scatter plot to see how the following factors affect a car's price: "age","odometer", "cyclinders","days_listed","model_year"
+st.write("""#####  Choose below the car characteristic and how it affects price for the Scatter Plot Chart""")
+Scatter_DropdownChoice = ["age","odometer", "cylinders","days_listed","model_year"]
+# since the following columns contain null values,(age, odometer, cyclinders, and model_year) we will clean our main df_Cars dataframe of null values
+# and use the resultant dataframe,(df_cleaned), for our scatter plot.
+df_cleaned =df_Cars.dropna()
+
+Scatter_choice = st.selectbox("Select one of the choices to compare against price:",Scatter_DropdownChoice)
+#the colors will be based on the make of the car.
+Fig_Scatter = px.scatter(df_cleaned, x="price", y=Scatter_choice, color = "make",hover_data="model", width=2000, height=500)
+Fig_Scatter.update_layout(title="<b> {} vs Price</b>".format(Scatter_choice))
+# we will limit the x axis range to 100,000 just to make the graph look bigger and better proportioned
+Fig_Scatter.update_xaxes(range=[0, 100000])
+st.plotly_chart(Fig_Scatter)
+
+# THE WEB APP IS NOW COMPLETE.  First portion of the web app filters a dataframe.  Second portion displays an interactive Histogram
+# and Scatter-plot graph
